2cae3a824169c6eae3d77f76f2d6a160d2222910
Kirti Kamtikar <kkamtikar@users.noreply.github.com>
2014-12-20 10:37:00 -0500

    Additional comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,46 @@
-# getwd()
-# setwd to the desired location. "UCI HAR Dataset" is assumed to be the working directory for all the paths.
+# Created Date: 19-Dec-2014
+# Created by: Kirti Kamtikar
+# https://github.com/kkamtikar/
 
-# Refer to the README for details of the steps involved in this analysis and codeBook for the details of the variables.
+# Refer to the README for the project background and files used in the project.
+# Refer to the CodeBook.md for details of the steps involved in this analysis and 
+# details of the variables used in this analysis and final output - TidyData.
+
+# As described in the README.md, make sure that the zipped file containing data files is downloaded and extracted.
+# UCI HAR Dataset contains train and test folders with all the specified data files.
+
+# getwd()
+# setwd to the desired location.
+# "UCI HAR Dataset" is assumed to be the working directory for all the paths.
+# Set the working directory and/ or all the paths for read.table statements appropriately.
+
 #-------------------------------------------------------------------------------------------------------------------------
-# PART 1
+# PART 1 - Merges the training and the test sets to create one data set.
 #-------------------------------------------------------------------------------------------------------------------------
+
+# Read training and test data files from train and test folder respectively (data filenames starting with X_)
 xtrain <- read.table("./train/X_train.txt", header=FALSE, sep="")
 xtest <- read.table("./test/X_test.txt", header=FALSE, sep="")
+# Combine the rows of the training and test data
 clubx <- rbind(xtrain,xtest)
 
+# Read activity data for training and test from train and test folder respectively (data filenames starting with y_)
 ytrain <- read.table("./train/y_train.txt", header=FALSE, sep="")
 ytest <- read.table("./test/y_test.txt", header=FALSE, sep="")
+# Combine the rows of the training and test activity data
 cluby <- rbind(ytrain, ytest)
 
+# Read Subject IDs for training and test data from train and test folder respectively (data filenames starting with subject_)
 subTrain <- read.table("./train/subject_train.txt", header=FALSE, sep="")
 subTest <- read.table("./test/subject_test.txt", header=FALSE, sep="")
+# Combine the rows of the training and test Subjects data
 clubSub <- rbind(subTrain, subTest)
 
 # Combine all the columns for all the three data sets 
 mergeDataSet <- cbind(cbind(cluby, clubSub), clubx)
+# mergeDataSet contains features data, activity and subject data combined for training and test sets.
 
-#Read features from features.txt file
+# Read features from features.txt file
 features <- read.table("./features.txt", header=FALSE, sep="")
 
 # features data frame contains identifier and variable / feature name.
@@ -28,12 +48,12 @@ features <- read.table("./features.txt", header=FALSE, sep="")
 # It will be a factor type and hence needs to be converted to character to create a list of names
 featuresNames <- c("Activity", "SubjectID",as.character(features[,2]))
 
-#Assign these featuresNames as column names to the mergeDataSet data frame
+# Assign these featuresNames as column names to the mergeDataSet data frame
 colnames(mergeDataSet) <- featuresNames
 
 
 #-------------------------------------------------------------------------------------------------------------------------
-# PART 2
+# PART 2 - Extracts only the measurements on the mean and standard deviation for each measurement. 
 #-------------------------------------------------------------------------------------------------------------------------
 
 # Create a subset of the features for mean
@@ -52,12 +72,12 @@ stdSet <- mergeDataSet[,grep("std()", colnames(mergeDataSet),fixed=TRUE)]
 act_sub <- mergeDataSet[,grep("Activity|SubjectID", colnames(mergeDataSet))]
 
 # The order of combing the columns together is important. 
-#The first two columns correspond to Activity and SubjectID, followed by columns for mean features and then std features.
+# The first two columns correspond to Activity and SubjectID, followed by columns for mean features and then std features.
 mean_std_set <- cbind(cbind(act_sub,meanSet),stdSet)
 
 
 #-------------------------------------------------------------------------------------------------------------------------
-# PART 3
+# PART 3 - Uses descriptive activity names to name the activities in the data set
 #-------------------------------------------------------------------------------------------------------------------------
 # Read activity data from activity_labels.txt
 activityData <- read.table("./activity_labels.txt", header=FALSE, sep="")
@@ -71,7 +91,7 @@ mean_std_set$Activity <- factor(mean_std_set$Activity, levels=c(1,2,3,4,5,6), la
 
 
 #-------------------------------------------------------------------------------------------------------------------------
-# PART 4
+# PART 4 - Appropriately labels the data set with descriptive variable names. 
 #-------------------------------------------------------------------------------------------------------------------------
 # Make the column names more descriptive
 
@@ -88,7 +108,8 @@ colnames(mean_std_set) <- desc_labels
 # mean_std_set contains all the data with descriptive frequency names
 
 #-------------------------------------------------------------------------------------------------------------------------
-# PART 5
+# PART 5 - From the data set in step 4, creates a second, independent tidy data set with the 
+# average of each variable for each activity and each subject.
 #-------------------------------------------------------------------------------------------------------------------------
 # For each SubjectID and Activity, value for each variable will be listed
 library(reshape2)
@@ -98,6 +119,6 @@ library(dplyr)
 # Group by Subject ID, Activity and feature name and perform average on the values and create tidyData set.
 tidyData <- summarise(group_by(dataMelt, SubjectID, Activity,variable), average=mean(value))
 
-
 # Using write.table, write the tidy data set to a text file with row.names set to FALSE
+# The file TidyData.txt will be created in the current working directory.
 write.table(tidyData,file="./TidyData.txt",row.names=FALSE, sep=" ")
